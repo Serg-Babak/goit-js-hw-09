f8de166647a948af34c20c7d93e44adcc4ba3de5
Serg-Babak <babakserg1@gmail.com>
2022-12-09 23:24:08 +0200

    task#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector("data-start");
+const stopBtn = document.querySelector("data-stop");
+const body = document.querySelector("body");
 
+startBut.addEventListener('click', () => {
+    id = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBut.setAttribute('disabled', '');
+    if (stopBut.hasAttribute('disabled')) stopBut.removeAttribute('disabled');
+  });
+  
+  stopBut.addEventListener('click', () => {
+    clearInterval(id);
+    stopBut.setAttribute('disabled', '');
+    startBut.removeAttribute('disabled');
+  });
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
